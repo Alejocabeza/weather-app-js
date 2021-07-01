d169845136c32a4ed78fc41d1ff908b5f7377ae9
Alejandro Cabeza <alejandrocabeza400@gmail.com>
2021-06-30 23:56:22 -0400

    30/jun/2021

---
+++ src/app/weather.js
@@ -0,0 +1,23 @@
+'use strict'
+
+
+export class Weather {
+  constructor(city, countryCode){
+    this.city = city;
+    this.countryCode = contryCode;
+    this.apiKey = "2162b9b02534035ed4c2687bee11ba9f"
+  }
+
+  async getweather(){
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${this.city},${this.countryCode}&appid=${this.apiKey}&units=metric`;
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  }
+
+  changeLocation(city, countryCode){
+    this.city = city;
+    this.countryCode = countryCode;
+  }
+}
+
